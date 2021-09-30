09bfe53e6fe86372c1f99167743c3e5209d0e15d
matteo-andreuzza <76852004+matteo-andreuzza@users.noreply.github.com>
2021-09-30 22:05:10 +0200

    inserita la possibilità di far avanazare il programma anche dopo la restituzione del risultato. aumentatai i tempi con time

--- main.py
+++ main.py
@@ -1,29 +1,31 @@
 #import
 import time
-
+import os
 print("benvenuto nel calcolatore autmatico dell'MCD")
 print("versione 0.0.1")
 time.sleep(1)
+cor = input("se vuoi che il programma non si chiuda dopo aver restituito il risultato, scrivi 'ok', altrimenti premi invio")
 print("per prima cosa inseriamo i numeri di cui vuoi calcolare l'MCD")
-#definizione varibili
-a1 = int(input('inserisci il primo numero  '))
-b1 = int(input('inserisci il secondo numero  '))
-a = a1
-b = b1
-print('grazie! ora eseguo i calcoli')
-time.sleep(1)
-
-
-#algoritmo
-
-while a != b:
-    if a > b :
-        a = a-b
-    if b > a :
-        b = b-a
+while True:
+    #definizione varibili
+    a1 = int(input('inserisci il primo numero  '))
+    b1 = int(input('inserisci il secondo numero  '))
+    a = a1
+    b = b1
+    print('grazie! ora eseguo i calcoli')
+    time.sleep(1)
     
-print("ho finito!")
-print("L'MCD tra " + str(a1) + " e " + str(b1) + " è: " + str(a))
+    #algoritmo
+    while a != b:
+        if a > b :
+            a = a-b
+        if b > a :
+            b = b-a
+    
+    time.sleep(2)
+    print("ho finito!")
+    print("L'MCD tra " + str(a1) + " e " + str(b1) + " è: " + str(a))
 
-
-
+    if cor != 'ok':
+        break
+input('premi invio per uscire')
